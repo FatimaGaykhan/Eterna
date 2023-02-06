c73aadb04c47d08706f050e9af461fb32bb75e33
FatimaGaykhan <fatimaeg@code.edu.az>
2023-02-07 00:09:09 +0400

    First Commit

---
+++ Eterna/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Eterna.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eterna.DataAccessLayer
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+		public DbSet <Pricing> Pricings { get; set; }
+
+
+
+	}
+}
+
---
+++ Eterna/Models/Pricing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Eterna.Models
+{
+	public class Pricing
+	{
+		public int Id { get; set; }
+		[StringLength(100),Required]
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public bool IsFeatured { get; set; }
+
+
+	}
+}
+
